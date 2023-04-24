806a5ccdf9e6447211de9a08c85373ea7b9713c7
HareemSaad <hareems20014@outlook.com>
2023-04-24 16:36:38 +0500

    added credit form

--- front-end/src/components/general-journal/index.js
+++ front-end/src/components/general-journal/index.js
@@ -17,29 +17,47 @@ import './style.css';
 // }]
 
 export const GeneralJournal = () => {
-    const [transactions, setTransactions] = useState([{ 
+    const [debitTransactions, setDebitTransactions] = useState([{ 
         flag : 'N', 
         account : 101, 
         decription : '', 
-        debitTransaction : false, 
-        creditTransaction : false, 
-        debitAmount : 0, 
+        debitTransaction : false,
+        debitAmount : 0
+    }]);
+
+    const [creditTransactions, setCreditTransactions] = useState([{ 
+        flag : 'N', 
+        account : 101, 
+        decription : '',  
+        creditTransaction : false,
         creditAmount : 0 
     }]);
 
     useEffect (() => {
-        console.log(transactions);
-    }, [transactions])
+        console.log(debitTransactions);
+    }, [debitTransactions])
 
     // on submit check if sum debit == sum credit
 
-    const updateTransactionList = async (index, name, value) => {
-        let data = Object.assign([], transactions);
+    const updateDebitTransactionList = async (index, name, value) => {
+        let data = Object.assign([], debitTransactions);
         if (typeof name === "object") {
           data[index] = { ...data[index], ...name };
         } else if (name === 'debitAmount') {
             data[index]['debitTransaction'] = true
             data[index][name] = parseFloat(value);
+        } else {
+          data[index][name] = value;
+        }
+
+        console.log("data", data);
+        setDebitTransactions(data);
+    };
+
+    const updateCreditTransactionList = async (index, name, value) => {
+        let data = Object.assign([], creditTransactions);
+        if (typeof name === "object") {
+          data[index] = { ...data[index], ...name };
         } else if (name === 'creditAmount') {
             data[index]['creditTransaction'] = true;
             data[index][name] = parseFloat(value);
@@ -48,43 +66,59 @@ export const GeneralJournal = () => {
         }
 
         console.log("data", data);
-        setTransactions(data);
-      };
+        setCreditTransactions(data);
+    };
 
-    const removeRow = (i) => {
-        const newTransactions = [...transactions];
+    const removeDebitRow = (i) => {
+        const newTransactions = [...debitTransactions];
         if (newTransactions.length == 1) {return}
         newTransactions.splice(i, 1);
-        setTransactions(newTransactions);
-      };
+        setDebitTransactions(newTransactions);
+    };
 
-    const addRow = (e) => {
-        if (transactions.length == 3) {return}
-        setTransactions([...transactions, {
+    const removeCreditRow = (i) => {
+        const newTransactions = [...creditTransactions];
+        if (newTransactions.length == 1) {return}
+        newTransactions.splice(i, 1);
+        setCreditTransactions(newTransactions);
+    };
+
+    const addDebitRow = (e) => {
+        if (debitTransactions.length == 3) {return}
+        setDebitTransactions([...debitTransactions, {
             flag: 'N',
             account: 101,
             description: '',
             debitTransaction: false,
+            debitAmount: 0
+        }]);
+    };
+
+    const addCreditRow = (e) => {
+        if (creditTransactions.length == 3) {return}
+        setCreditTransactions([...creditTransactions, {
+            flag: 'N',
+            account: 101,
+            description: '',
             creditTransaction: false,
-            debitAmount: 0,
             creditAmount: 0
         }]);
     };
 
-    const tokenRow = (data, index) => {
+    const debitTokenRow = (data, index) => {
         return (
         <tr key={index}>
             <td>
             <button
                 className="btn text-danger fa fa-remove"
-                onClick={(e) => removeRow(index)}
+                onClick={(e) => removeDebitRow(index)}
             ></button>
             </td>
             <td className="w-25">
             <FormControl className="textfield MuiTextField-root mb-3">
                 <Select
                 value={data?.flag}
-                onChange={(e) => updateTransactionList(index, "flag", e.target.value)}
+                onChange={(e) => updateDebitTransactionList(index, "flag", e.target.value)}
                 className="textfield"
                 variant="standard"
                 required
@@ -100,7 +134,7 @@ export const GeneralJournal = () => {
             <FormControl className="textfield MuiTextField-root mb-3">
                 <Select
                 value={data?.account}
-                onChange={(e) => updateTransactionList(index, "account", e.target.value)}
+                onChange={(e) => updateDebitTransactionList(index, "account", e.target.value)}
                 className="textfield"
                 variant="standard"
                 required
@@ -115,7 +149,7 @@ export const GeneralJournal = () => {
             <td>
             <TextField
                 value={data?.description}
-                onChange={(e) => updateTransactionList(index, "description", e.target.value)}
+                onChange={(e) => updateDebitTransactionList(index, "description", e.target.value)}
                 className="textfield"
                 variant="standard"
                 required
@@ -123,8 +157,71 @@ export const GeneralJournal = () => {
             </td>
             <td>
             <TextField
-                value={data?.debit}
-                onChange={(e) => updateTransactionList(index, "debitAmount", e.target.value)}
+                value={data?.debitAmount}
+                onChange={(e) => updateDebitTransactionList(index, "debitAmount", e.target.value)}
+                className="textfield"
+                variant="standard"
+                required
+            />
+            </td>
+        </tr>
+        );
+    };
+
+    const creditTokenRow = (data, index) => {
+        return (
+        <tr key={index}>
+            <td>
+            <button
+                className="btn text-danger fa fa-remove"
+                onClick={(e) => removeCreditRow(index)}
+            ></button>
+            </td>
+            <td className="w-25">
+            <FormControl className="textfield MuiTextField-root mb-3">
+                <Select
+                value={data?.flag}
+                onChange={(e) => updateCreditTransactionList(index, "flag", e.target.value)}
+                className="textfield"
+                variant="standard"
+                required
+                label="Flag"
+                >
+                <MenuItem value="A" name='ADJ'>Adjustment</MenuItem>
+                <MenuItem value="C" name='CLO'>Closing</MenuItem>
+                <MenuItem value="N" name='NOR'>Normal</MenuItem>
+                </Select>
+            </FormControl>
+            </td>
+            <td className="w-25">
+            <FormControl className="textfield MuiTextField-root mb-3">
+                <Select
+                value={data?.account}
+                onChange={(e) => updateCreditTransactionList(index, "account", e.target.value)}
+                className="textfield"
+                variant="standard"
+                required
+                label="Account"
+                >
+                <MenuItem value="101" name='101'>101</MenuItem>
+                <MenuItem value="102" name='102'>102</MenuItem>
+                <MenuItem value="103" name='103'>103</MenuItem>
+                </Select>
+            </FormControl>
+            </td>
+            <td>
+            <TextField
+                value={data?.description}
+                onChange={(e) => updateCreditTransactionList(index, "description", e.target.value)}
+                className="textfield"
+                variant="standard"
+                required
+            />
+            </td>
+            <td>
+            <TextField
+                value={data?.creditAmount}
+                onChange={(e) => updateCreditTransactionList(index, "creditAmount", e.target.value)}
                 className="textfield"
                 variant="standard"
                 required
@@ -147,7 +244,7 @@ export const GeneralJournal = () => {
                         <th>
                             <button
                             className="btn text-success fa fa-plus-circle"
-                            onClick={addRow}
+                            onClick={addDebitRow}
                             ></button>
                         </th>
                         <th>Flag</th>
@@ -157,13 +254,13 @@ export const GeneralJournal = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {transactions.map((item, index) => {return tokenRow(item, index);})}
+                        {debitTransactions.map((item, index) => {return debitTokenRow(item, index);})}
                     </tbody>
                     </table>
                 </div>
                 </div>
             </div>
-            {/* <div className="erc20-cont">
+            <div className="erc20-cont">
                 <div className="erc20-inner-cont">
                 <div className="erc20-">
                     <table className="table table-bordered">
@@ -172,22 +269,22 @@ export const GeneralJournal = () => {
                         <th>
                             <button
                             className="btn text-success fa fa-plus-circle"
-                            onClick={addRow}
+                            onClick={addCreditRow}
                             ></button>
                         </th>
                         <th>Flag</th>
                         <th>Account</th>
                         <th>Decription</th>
-                        <th>Debit</th>
+                        <th>Credit</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {transactions.map((item, index) => {return tokenRow(item, index);})}
+                        {creditTransactions.map((item, index) => {return creditTokenRow(item, index);})}
                     </tbody>
                     </table>
                 </div>
                 </div>
-            </div> */}
+            </div>
             </div>
         </>
     );
